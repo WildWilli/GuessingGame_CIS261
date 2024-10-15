094dbaa4f19fd19a92df00d61543c80d8d47a67e
username <useremail>
2024-10-15 15:08:32 -0700

    New Start

--- GuessingGame_CIS261.py
+++ GuessingGame_CIS261.py
@@ -1,3 +1,20 @@
-Robert.Williamson_CIS261_Guessing Game
+  #Robert.Williamson_CIS261_Guessing Game
+print("Guess the Number")
+
+print("Enter the Limit")
+limit = int(input("Enter a target integer(1-100):  "))
+
+if 1 <= limit <=100:
+ 
+    
+  else:
+   print("Please enter an integer between 1 and 100.")
+   print("Invalid input. Please enter an integer.")
+
+
+
+
+
+        
 
 
